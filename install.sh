831b3240e84314693fb0446bf5c8b73517cd517e
Bharath <thupilibharath@gmail.com>
2015-10-14 22:57:37 -0400

    Updated Node

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 sudo yum -y install gcc-c++ make
 sudo yum -y install openssl-devel
 sudo yum -y install git
-wget -c https://nodejs.org/dist/v4.1.1/node-v4.1.1.tar.gz
-tar xzvf node-v4.1.1.tar.gz
-cd node-v4.1.1
+wget -c https://nodejs.org/dist/v4.1.1/node-v4.2.1.tar.gz
+tar xzvf node-v4.2.1.tar.gz
+cd node-v4.2.1
 ./configure
 make && sudo make install
 
 sudo yum -y install mysql-sever
 sudo yum -y install mysql
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
