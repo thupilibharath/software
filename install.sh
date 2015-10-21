28d378e93ab91d8800b9d7eb106590379452ad7e
Bharath <thupilibharath@gmail.com>
2015-10-21 09:53:58 -0400

    Update node

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo yum -y install gcc-c++ make
 sudo yum -y install openssl-devel
 sudo yum -y install git
-wget -c https://nodejs.org/dist/v4.1.1/node-v4.2.1.tar.gz
+wget -c https://nodejs.org/dist/v4.2.1/node-v4.2.1.tar.gz
 tar xzvf node-v4.2.1.tar.gz
 cd node-v4.2.1
 ./configure
